956e4d49a22c77991e7c76efc281a86c8a5fa77d
Stu Cowley <stucowley@outlook.com>
2023-06-09 15:24:50 +0930

    Make control text <svg> not <img>

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ var slider = tns({
   controls: true,
   controlsPosition: 'bottom',
   controlsText: [
-    '<img src="../assets/images/icon-arrow-left.svg" alt />',
-    '<img src="../assets/images/icon-arrow-right.svg" alt />',
+    '<svg width="15" height="16" xmlns="http://www.w3.org/2000/svg"><path fill="#FFF" d="M8 0 0 8l8 8 1.687-1.687-5.121-5.12h10.241V6.807H4.566l5.121-5.12z"/></svg>',
+    '<svg width="15" height="16" xmlns="http://www.w3.org/2000/svg"><path fill="#FFF" d="m6.808 16 8-8-8-8-1.687 1.687 5.121 5.12H.001v2.386h10.241l-5.121 5.12z"/></svg>',
   ],
 });
