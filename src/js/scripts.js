4f5be007f2e892e33ce240486fb6f6dbe3220ff2
Stu Cowley <stucowley@outlook.com>
2025-07-11 17:38:13 +0930

    feat(scripts.js): add fade in script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,3 +13,30 @@
 
 // Test scipts.js is loaded
 console.log('Hello from scripts.js');
+
+/**
+ * FADE IN EFFECT
+ * 
+ * Uses IntersectionObserver to apply the `fade-in-active` class to elements 
+ * with the `.fade-in` class when they come into view, triggering a fade-in 
+ * effect.
+ * 
+ * The observer watches each `.fade-in` element and adds the `fade-in-active` 
+ * class which it becomes 10% visible in the viewport. After the class is 
+ * added, the element is no longer observed.
+ */
+
+const fadeInElements = document.querySelectorAll('.fade-in');
+const fadeInObserver = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in__active');
+                fadeInObserver.unobserve(entry.target);
+            }
+        });
+    },
+    { threshold: 0.1 }
+);
+
+fadeInElements.forEach((element) => fadeInObserver.observe(element));
